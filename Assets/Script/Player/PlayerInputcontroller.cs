6cf427ed3df5301f87484027fc8c07e5139c3e0f
addmati <audtn0409@naver.com>
2024-06-19 13:34:41 +0900

    Feat
    Player폴더에 스크립트추가

---
+++ Assets/Script/Player/PlayerInputcontroller.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+public class   PlayerInputcontroller  : MoveController
+{
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+}
