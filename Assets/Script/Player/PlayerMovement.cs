56f8ce2f88382eef081bd066944fc0f96d54978e
addmati <audtn0409@naver.com>
2024-06-19 19:51:11 +0900

    Feat
    경사면에 있을때 내려가지않게 해노았습니다.

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerMovement : MonoBehaviour
 {
-   
+
 
 
     public Rigidbody2D rb;
 
     private float horizontal;
-    private float speed = 8f;
-  
+    private float speed = 3f;
+
     void Update()
     {
-   
 
-    
+        if (!Input.anyKey)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX | 
+                RigidbodyConstraints2D.FreezeRotation;
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
 
+        }
 
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }  
-    
+    }
+
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
