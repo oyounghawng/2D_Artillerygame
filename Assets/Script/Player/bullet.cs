cbe940bb8a40c9465adaf60f5489f5a3221adad2
addmati <audtn0409@naver.com>
2024-06-21 14:11:15 +0900

    Add: 탱크 파워게이지 추가

--- Assets/Script/Player/bullet.cs
+++ Assets/Script/Player/bullet.cs
@@ -6,18 +6,21 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     Rigidbody2D rb;
-    public float bulletSpeed ;
+    public float bulletSpeed;
+    public Gazer gazer;
  
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        gazer = GameObject.FindObjectOfType<Gazer>();
+        bulletSpeed = gazer.Force;
         rb.AddForce(PlayerAttack.Gunangle.transform.right * bulletSpeed);
     }
     
     void FixedUpdate()
-    { 
-        float angle=Mathf.Atan2(rb.velocity.y, rb.velocity.x)*Mathf.Rad2Deg;
-        transform.eulerAngles= new Vector3 (0f, 0f, angle);
+    {
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0f, 0f, angle);
     }
 }
